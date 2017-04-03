db41c80ea83349e54155c4be12ee9ee2825bb8ea
Unknown <vs.kamyshev@gmail.com>
2017-04-03 15:27:19 +0300

    Функция разбита на три

--- PY3-2.6.py
+++ PY3-2.6.py
@@ -3,14 +3,21 @@ import glob
 import os.path
 import shutil
 
-def copy_and_resize (file_list, directory):
+folder_name = 'Result'
+
+def make_folder():
     #создание папки для полученных изображений
-    if not os.path.isdir('./Result'):
-        os.mkdir('./Result')
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+
+def resize_img(filename, directory):
+    subprocess.run(["sips", "--resampleWidth", "200", os.path.join(directory, folder_name, filename.split('/')[1])])
+
+def copy_and_resize (file_list, directory):
     #цикл, копирующий изображения и изменяющий размер
     for f in file_list:
-        shutil.copy(os.path.join(directory, f), os.path.join(directory, "Result", f.split('/')[1]))
-        subprocess.run(["sips", "--resampleWidth", "200", os.path.join(directory, "Result", f.split('/')[1])])
+        shutil.copy(os.path.join(directory, f), os.path.join(directory, folder_name, f.split('/')[1]))
+        resize_img(f)
 
 file_list = glob.glob(os.path.join("Source", "*.jpg")) #список исходных изображений
 current_path = os.getcwd() #текущая папка
